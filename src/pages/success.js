f937b30cee4ff541689e5f47b79a3af1d067ef94
David Tonge <contact@davidtonge.me>
2022-03-19 12:02:04 -0400

    added success page

---
+++ src/pages/success.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Success = () => (
+  <Layout>
+    <Seo title="Success" />
+    <h1 className="title">Thank you for contacting CFEG...</h1>
+  </Layout>
+)
+
+export default Success
